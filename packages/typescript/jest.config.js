31c55403cfe74ca3a6b4ec2eabee12bccc6a98be
Quentin Pigné <quentin.pigne@gmail.com>
2026-01-31 12:20:02 +0100

    chore(typescript): migrating from Jest to Vitest

--- packages/typescript/jest.config.js
+++
@@ -1,9 +0,0 @@
-import { createDefaultEsmPreset } from "ts-jest";
-
-const tsJestDefaultEsmPreset = createDefaultEsmPreset();
-
-export default {
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
-  ...tsJestDefaultEsmPreset,
-};
